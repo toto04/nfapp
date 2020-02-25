5844d06de997e2c561cdb1100caea4e90427ecec
Tommaso Morganti <totoscemo@gmail.com>
2020-02-25 02:39:36 +0100

    icon & build

--- pages/Settings/index.tsx
+++ pages/Settings/index.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Page, NavigationProps } from '../../util'
-import { StyleSheet, Text, StyleProp, TextStyle } from 'react-native'
+import { StyleSheet, Text, StyleProp, TextStyle, Alert, Linking } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { connect } from 'react-redux'
 import { logout } from '../../redux/login'
@@ -33,10 +33,13 @@ class _SettingsPage extends Component<NavigationProps & { logout: typeof logout 
         <Option>Cambia password</Option>
         <Option>Cambia indirizzo email</Option>
         <Text style={styles.section}>Altro</Text>
-        <Option>Contattaci</Option>
+        <Option onPress={() => {
+            Alert.alert('Contattaci', 'Puoi contattare i Rappresentati di Istituto o gli Sviluppatori dell\'applicazione via Whatsapp o email ai recapiti che trovi in fondo alla Politica della Privacy', [{ text: 'Vai ai recapiti', onPress: () => Linking.openURL('https://nfapp-server.herokuapp.com/privacy') }, { text: 'Annulla', style: 'cancel' }])
+        }}>Contattaci</Option>
         <Option onPress={() => {
             this.props.logout()
             this.props.navigation.navigate('Profile')
+            Alert.alert('Logout', 'Probabilmente te ne sei accorto ma hai effettuato il logout')
         }} style={{ color: 'red' }}>Logout</Option>
     </Page>
 }
